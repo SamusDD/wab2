8002785f6f4a9838f13a6d24ae334b5f7b1edbce
SamusDD <samueldannatt@gmail.com>
2022-03-10 20:36:43 +0000

    adding minor changes for branch switch

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
-
+from flask import Flask, redirect, url_for
 wabapp = Flask(__name__)
+
+data = {}
 
 @wabapp.route('/')
 def home():
-    return "Hello Internet!"
+    return redirect(url_for('redir'))
+
+@wabapp.route('/redirect')
+def redir():
+    return "You have been successfully redirected"
 
 if __name__ == '__main__':
     wabapp.run(debug=True, host='0.0.0.0')
